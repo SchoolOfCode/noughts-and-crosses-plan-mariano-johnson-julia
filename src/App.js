f625d950b2d94bb564ca98e5c9f5bc052355f137
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-02-09 13:46:09 +0000

    Add current player turn to Game component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Game from "./Components/Game";
 
 function App() {
-  return <div className="App">Tic-Tac-Toe</div>;
+  return (
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
+      <Game />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Game/index.js
+++ src/Components/Game/index.js
@@ -5,7 +5,16 @@ const Game = () => {
   const [board, setBoard] = useState(initialBoard);
   const [turns, setTurns] = useState(false); // true is X's turn, false is O's turn
 
-  return <div></div>;
+  return (
+    <div>
+      Game Component
+      {turns ? (
+        <h2>It is Player X's turn.</h2>
+      ) : (
+        <h2>It is Player O's turn.</h2>
+      )}
+    </div>
+  );
 };
 
 export default Game;
